5ba867b201dd8d2a12bbb908e91735f8809d0200
shamim <shamim.pstu@gmail.com>
2021-12-14 00:29:51 +0600

    frontend

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Login from './Pages/Login/Login/Login';
 import Header from './Pages/Shared/Header/Header';
 import AuthProvider from './contexts/AuthProvider';
 import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
-import AddService from './Pages/AddService/addService';
+import AddService from './Pages/AddService/AddService'
+import ManageServices from './Pages/ManageService/ManageServices';
 
 
 function App() {
@@ -29,8 +30,11 @@ function App() {
             <PrivateRoute path="/booking/:serviceId">
               <Booking></Booking>
             </PrivateRoute>
-            <Route path = "/addService">
+            <Route path="/addService">
               <AddService></AddService>
+            </Route>
+            <Route path="/manageServices">
+              <ManageServices></ManageServices>
             </Route>
             <Route path="*">
               <NotFound></NotFound>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -5,7 +5,7 @@ import './Services.css';
 const Services = () => {
     const [services, setServices] = useState([])
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
@@ -15,10 +15,7 @@ const Services = () => {
             <h2 className="text-primary mt-5">Our services</h2>
             <div className="service-container">
                 {
-                    services.map(service => <Service
-                        key={service.id}
-                        service={service}
-                    ></Service>)
+                    services.map(service => <Service key={service.id} service={service}></Service>)
                 }
             </div>
         </div>
